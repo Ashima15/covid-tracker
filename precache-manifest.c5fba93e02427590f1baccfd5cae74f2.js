3dd23aab3eb1adde657e92ef30ee0eb3ec2b75c3
ashimaWalia <ashima@iimjobs.com>
2020-07-20 18:43:31 +0530

    Updates

---
+++ precache-manifest.c5fba93e02427590f1baccfd5cae74f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc5a6fed7fcf87025746af283f06c8f",
+    "url": "/Ashima15/covid-tracker/index.html"
+  },
+  {
+    "revision": "2851e9c58b71269043df",
+    "url": "/Ashima15/covid-tracker/static/css/main.574acc90.chunk.css"
+  },
+  {
+    "revision": "3fbaed71dcf508f95ee3",
+    "url": "/Ashima15/covid-tracker/static/js/2.0233c61b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Ashima15/covid-tracker/static/js/2.0233c61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2851e9c58b71269043df",
+    "url": "/Ashima15/covid-tracker/static/js/main.55f3f791.chunk.js"
+  },
+  {
+    "revision": "e4ab29363f34715a1b97",
+    "url": "/Ashima15/covid-tracker/static/js/runtime-main.6c005187.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Ashima15/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
